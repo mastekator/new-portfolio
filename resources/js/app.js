69ac6a7f832745ba1e967601f7f309f7d8444683
Maxim Tretiak <48452049+mastekator@users.noreply.github.com>
2020-08-18 21:34:46 +0300

    laravel-react init

---
+++ resources/js/app.js
@@ -0,0 +1,12 @@
+//React
+import React from 'react'
+import ReactDOM from 'react-dom';
+
+//App
+import Index from './index'
+
+const application = (
+    <Index/>
+);
+
+ReactDOM.render(application, document.getElementById('root'));
